8adbc4f8d544b389ac7a9263232e67d2972c2c1b
Lawrence Dovin <lawrencedovin@gmail.com>
2022-01-31 22:32:01 -0500

    added initial dumb components

--- 6-component-architecture-and-feature-modules/src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ 6-component-architecture-and-feature-modules/src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+
+// components
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 
 
 
 @NgModule({
   declarations: [
-    PassengerDashboardComponent
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
   ],
   imports: [
     CommonModule
