b79a6de1611cde27fd3259f8b7ae958d20adac0b
Lawrence Dovin <lawrencedovin@gmail.com>
2022-02-16 16:47:52 -0500

    Added checkbox, radio button example

--- 8-template-driven-forms/src/app/passenger-dashboard/containers/passenger-form/passenger-form.component.ts
+++ 8-template-driven-forms/src/app/passenger-dashboard/containers/passenger-form/passenger-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { Passenger } from 'src/app/interfaces/passenger.interface';
+import { Baggage } from 'src/app/interfaces/baggage.interface';
 
 
 @Component({
@@ -10,7 +12,32 @@ import { Passenger } from 'src/app/interfaces/passenger.interface';
 export class PassengerFormComponent implements OnInit {
 
   @Input()
-  detail: Passenger
+  detail: Passenger;
+
+  baggage: Array<Baggage> = [
+    {
+      key: 'none',
+      value: 'No baggage'
+    },
+    {
+      key: 'hand-only',
+      value: 'Hand baggage'
+    },
+    {
+      key: 'hold-only',
+      value: 'Hold baggage'
+    },
+    {
+      key: 'hand-hold',
+      value: 'Hand and Hold baggage'
+    },
+  ]
+
+  toggleCheckIn(checkedIn: boolean) {
+    if(checkedIn) {
+      this.detail.checkedInDate = Date.now();
+    }
+  }
 
   constructor() { }
 
