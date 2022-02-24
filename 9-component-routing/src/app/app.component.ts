455f91825d019b58febc78b6aa560cfc7f8ec084
Lawrence Dovin <lawrencedovin@gmail.com>
2022-02-24 17:11:28 -0500

    Added dynamic navigation with ngFor

--- 9-component-routing/src/app/app.component.ts
+++ 9-component-routing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Nav } from './interfaces/nav.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  nav: Array<Nav> = [
+    {
+      link: '/',
+      name: 'Home',
+      exact: true
+    },
+    {
+      link: '/oops',
+      name: '404',
+      exact: false
+    },
+  ]
 }
