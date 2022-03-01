c13beef7aa4cb0065ee94a2bf8b87ce16804e291
Lawrence Dovin <lawrencedovin@gmail.com>
2022-03-01 16:37:31 -0500

    Added initial child and dynamic routes for passenger by id

--- 9-component-routing/src/app/passenger-dashboard/passenger-dashboard.routing.ts
+++ 9-component-routing/src/app/passenger-dashboard/passenger-dashboard.routing.ts
@@ -2,9 +2,15 @@ import { NgModule } from  '@angular/core';
 import { Routes, RouterModule } from  '@angular/router';
 
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
 
 const routes: Routes = [
-  { path: 'passengers', component: PassengerDashboardComponent }
+  { path: 'passengers',
+    children: [
+      { path: '', component: PassengerDashboardComponent },
+      { path: ':id', component: PassengerViewerComponent }
+    ]
+  }
 ];
 
 @NgModule({
